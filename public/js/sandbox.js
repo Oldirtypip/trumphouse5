299451569ad79891dd1f971cc6793502667d586b
student <student@ai251lab0216m16.aicala.edmc.stu>
2016-10-11 11:03:09 -0700

    third push

---
+++ public/js/sandbox.js
@@ -0,0 +1,21 @@
+console.log('game ready');
+
+function counter ( amount ) {
+
+	if ( isNaN(amount) == true ) {
+		console.log( "Please write a number!");
+
+		return;
+	} // end o ftest for string
+
+	if ( amount < 1 ) {
+		console.com
+	}
+
+for (var i = 0; i < amount; i++) {
+
+	console.log( "counted to: " + i );
+
+	}
+
+}
